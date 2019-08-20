e511755e6845b4dfa893e58f21d457f28698155e
Maxime <mx.dufresne@gmail.com>
2019-08-20 13:39:32 -0400

    Clouds, Shooting Star, Star Trails

--- Qatar_Prototype/Assets/Scripts/Sky/SkyStarNavigation.cs
+++ Qatar_Prototype/Assets/Scripts/Sky/SkyStarNavigation.cs
@@ -10,14 +10,14 @@ public class SkyStarNavigation : MonoBehaviour
 	private void Start()
 	{
 		altitude = transform.eulerAngles.x;
-		lyDistance = Random.Range(0.05f, 1f);
+		lyDistance = Random.Range(0f, 1f);
 	}
 
 	private void Update()
 	{
 		float lyDistanceSpeedMultiplier = 10f;
-		float altitudeSpeedMultiplier = 0.1f;
-		altitude += Time.deltaTime * (altitude * altitudeSpeedMultiplier) * ((1f - lyDistance) * lyDistanceSpeedMultiplier);
+		float altitudeSpeedMultiplier = 0.01f;
+		altitude += Time.deltaTime * (altitude * (altitude * (1f - lyDistance)) * altitudeSpeedMultiplier) * ((1f - lyDistance) * lyDistanceSpeedMultiplier);
 
 		if(altitude > 90)
 		{
@@ -30,6 +30,6 @@ public class SkyStarNavigation : MonoBehaviour
 
 		float lyDistanceSizeMultiplier = 2f;
 		float altitudeSizeMultiplier = 5f;
-		transform.GetChild(0).localScale = Vector3.one * (1200 * (altitudeSizeMultiplier * altitude / 90f) * (lyDistanceSizeMultiplier * (1f - lyDistance)));
+		transform.GetChild(0).localScale = Vector3.one * Mathf.Max(600, (1200 * (altitudeSizeMultiplier * altitude / 90f) * (lyDistanceSizeMultiplier * (1f - lyDistance))));
 	}
 }
