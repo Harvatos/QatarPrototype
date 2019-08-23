a49e25451bd47fdf354761b0f099196fb053bbaf
AlexisHarvey <harvey.alexis@hotmail.com>
2019-08-23 18:03:59 -0400

    Door dissolve

---
+++ Qatar_Prototype/Assets/Scripts/Interactible/DoorDissolve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorDissolve : MonoBehaviour
+{
+	public bool dissolve;
+
+	private Material m;
+	private float timer;
+
+	private void Start()
+	{
+		m = GetComponent<Renderer>().material;
+		timer = 1f;
+	}
+
+	private void Update()
+	{
+		if(dissolve && (timer >= 0))
+		{
+			timer -= Time.deltaTime;
+			m.SetFloat("_AlphaClip", timer);
+		}
+	}
+}
